325839f8c1aba1ef8514c7f49a39ca073f891a58
Notebook <ednavanfisico@gmail.com>
2023-04-23 10:44:28 -0300

    commit secondary master

--- src/main/java/AcessLogin.java
+++ src/main/java/AcessLogin.java
@@ -1,7 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AcessLogin {
+    @Test
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver","C:\\Users\\Notebook\\Downloads\\geckodriver-v0.33.0-win32\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
