5527d7c9203d06b2b2e9f3e234468a4be59d7624
Notebook <ednavanfisico@gmail.com>
2023-04-23 10:29:13 -0300

    commit initial

---
+++ src/main/java/AcessLogin.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AcessLogin {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\Notebook\\Downloads\\geckodriver-v0.33.0-win32\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.mercadolivre.com.br/");
+
+    }
+}
